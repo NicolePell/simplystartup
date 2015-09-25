08e874dea2fdefe54d19a796bcac4c7faea771f8
NicolePell <pellicena@gmail.com>
2015-09-25 11:54:50 +0100

    updated production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   config.assets.initialize_on_precompile = false
   # Settings specified here will take precedence over those in config/application.rb.
